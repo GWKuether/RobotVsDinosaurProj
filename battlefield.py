a5c6ca3b30fad6b65083f2fbdff799b6e434f690
George Kuether <gwkuether@gmail.com>
2022-11-01 11:49:46 -0500

    Added the ability for the user to choose the weapon. Added a loop to make the battle phase more interesting

--- battlefield.py
+++ battlefield.py
@@ -11,32 +11,44 @@ class Battlefield():
     def run_game(self):
         self.display_welcome()
         self.battle_phase()
-        self.display_winner()
+        
 
     def display_welcome(self):
         print(f"Welcome to the death match of the century.\n{self.dinosaur_fighter.name} Vs. {self.robot_fighter.name}\nWho will survive?")
 
+    def choose_robots_weapon(self):
+        weapon_chosen = False
+        while weapon_chosen == False:
+            weapon_choice = input(f"Which weapon should {self.robot_fighter.name} use? {self.robot_fighter.active_weapon[0].name}, {self.robot_fighter.active_weapon[1].name}, or {self.robot_fighter.active_weapon[2].name}? ")
+            if weapon_choice == self.robot_fighter.active_weapon[0].name:
+                self.robot_fighter.attack(self.dinosaur_fighter)
+                weapon_chosen = True
+            elif weapon_choice == self.robot_fighter.active_weapon[1].name:
+                self.robot_fighter.attack_2(self.dinosaur_fighter)
+                weapon_chosen = True
+            elif weapon_choice == self.robot_fighter.active_weapon[2].name:
+                self.robot_fighter.attack_3(self.dinosaur_fighter)
+                weapon_chosen = True
+
+
     def battle_phase(self):
-        self.dinosaur_fighter.attack(self.robot_fighter)
-        print(f"{self.robot_fighter.name} has {self.robot_fighter.health} health left!")
-        self.robot_fighter.attack(self.dinosaur_fighter)
-        print(f"{self.dinosaur_fighter.name} has {self.dinosaur_fighter.health} health left!")
-        self.robot_fighter.attack_2(self.dinosaur_fighter)
-        print(f"{self.dinosaur_fighter.name} has {self.dinosaur_fighter.health} health left!")
-        self.dinosaur_fighter.attack(self.robot_fighter)
-        print(f"{self.robot_fighter.name} has {self.robot_fighter.health} health left!")
-        self.robot_fighter.attack_2(self.dinosaur_fighter)
-        print(f"{self.dinosaur_fighter.name} has {self.dinosaur_fighter.health} health left!")
-        self.dinosaur_fighter.attack(self.robot_fighter)
-        print(f"{self.robot_fighter.name} has {self.robot_fighter.health} health left!")
-        self.robot_fighter.attack_3(self.dinosaur_fighter)
-        print(f"{self.dinosaur_fighter.name} has {self.dinosaur_fighter.health} health left!")
-        self.dinosaur_fighter.attack(self.robot_fighter)
-        print(f"{self.robot_fighter.name} has {self.robot_fighter.health} health left!")
+        
+        while self.dinosaur_fighter.health > 0 and self.robot_fighter.health > 0:
+            self.dinosaur_fighter.attack(self.robot_fighter)
+            print(f"{self.robot_fighter.name} has {self.robot_fighter.health} health left!")
+            if self.robot_fighter.health <= 0:
+                self.display_winner()
+                break  
+            self.choose_robots_weapon()
+            print(f"{self.dinosaur_fighter.name} has {self.dinosaur_fighter.health} health left!")
+            if self.dinosaur_fighter.health <= 0:
+                self.display_winner()
+                break
+                 
 
 
     def display_winner(self):
-        if self.dinosaur_fighter.health == 0:
+        if self.dinosaur_fighter.health <= 0:
             print(f"{self.dinosaur_fighter.name} has no health left! They've been defeated!\n{self.robot_fighter.name} WINS!!!")
         else:
             print(f"{self.robot_fighter.name} has no health left! They've been defeated!\n{self.dinosaur_fighter.name} WINS!!!")
--- robot.py
+++ robot.py
@@ -6,18 +6,16 @@ class Robot():
     def __init__(self, name):
         self.name = name
         self.health = 100
-        self.active_weapon = Weapon("Bazooka", 25)
-        self.active_weapon_2 = Weapon("Buzz Saw", 15)
-        self.active_weapon_3 = Weapon("Car Bomb", 40)
+        self.active_weapon = [Weapon("Bazooka", 25), Weapon("Buzz Saw", 15), Weapon("Car Bomb", 40)]
 
     def attack(self, dinosaur):
-        print(f"Using his {self.active_weapon.name}, {self.name} deals {self.active_weapon.attack_power} damage!")
-        dinosaur.health -= self.active_weapon.attack_power
+        print(f"Using his {self.active_weapon[0].name}, {self.name} deals {self.active_weapon[0].attack_power} damage!")
+        dinosaur.health -= self.active_weapon[0].attack_power
 
     def attack_2(self, dinosaur):
-        print(f"Using his {self.active_weapon_2.name}, {self.name} deals {self.active_weapon_2.attack_power} damage!")
-        dinosaur.health -= self.active_weapon_2.attack_power
+        print(f"Using his {self.active_weapon[1].name}, {self.name} deals {self.active_weapon[1].attack_power} damage!")
+        dinosaur.health -= self.active_weapon[1].attack_power
 
     def attack_3(self, dinosaur):
-        print(f"Using his {self.active_weapon_3.name}, {self.name} deals {self.active_weapon_3.attack_power} damage!")
-        dinosaur.health -= self.active_weapon_3.attack_power
+        print(f"Using his {self.active_weapon[2].name}, {self.name} deals {self.active_weapon[2].attack_power} damage!")
+        dinosaur.health -= self.active_weapon[2].attack_power
